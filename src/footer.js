dbb24cf45e04b952964bfc5170b7763ac454b3c8
Selva <sellva3009@gmail.com>
2023-08-08 15:01:49 +0530

    footer changes

---
+++ src/footer.js
@@ -0,0 +1,83 @@
+export const footerlinks = [
+  {
+    id: 1,
+    title: "Categories",
+    categoryList: [
+        "Graphics & Design",
+        "Digital Marketing",
+        "Writing & Translation",
+        "Video & Animation",
+        "Music & Audio",
+        "Programming & Tech",
+        "Data",
+        "Business",
+        "Lifestyle",
+        "Photography",
+        "Sitemap"
+    ]
+  },
+     {
+    id: 2,
+    title: "About",
+    categoryList: [
+        "Press & News",
+        "Partnerships",
+        "Privacy Policy",
+        "Terms of Service",
+        "Intellectual Property Claims",
+        "Investor Relations",
+        "Contact Sales",
+    ]
+  },
+     {
+    id: 3,
+    title: "Support",
+    categoryList: [
+        "Help & Support",
+        "Trust & Safety",
+        "Selling on Liverr",
+        "Buying on Liverr",
+    ]
+  },
+     {
+    id: 4,
+    title: "Community",
+    categoryList: [
+        "Customer Success Stories",
+        "Community hub",
+        "Forum",
+        "Events",
+        "Blog",
+        "Influencers",
+        "Affiliates",
+        "Podcast",
+        "Invite a Friend",
+        "Become a Seller",
+        "Community Standards",
+    ]
+  },
+     {
+    id: 5,
+    title: "More From Fiverr",
+    categoryList: [
+        "Liverr Business",
+        "Liverr Pro",
+        "Liverr Logo Maker",
+        "Liverr Guides",
+        "Get Inspired",
+        "Liverr Select",
+        "ClearVoice",
+        "Liverr Workspace",
+        "Learn",
+        "Working Not Working",
+    ]
+  },
+];
+
+
+            
+
+
+            
+            
+
